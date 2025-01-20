b65e8c4b0859484eb225451498d966b9e13b2269
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2025-01-19 19:16:02 -0500

    fix: show calculator on mobile pt10

--- js/calc.js
+++ js/calc.js
@@ -51,7 +51,7 @@ const operate = function(operator, num1, num2) {
 const mainDisplay = document.getElementById("main-display");
 
 // Display content is 0;
-mainDisplay.textContent = 'Go Retro2';
+mainDisplay.textContent = 'Go Retro3';
 
 // Create an array that stores the display value
 let displayNumber = [];
